b99c7e70d9304f98936e3f603516ad3c3a155ec0
alekseyesev <alekseyesev@live.ru>
2020-02-26 17:54:27 +0300

    Story 8: Add slomux

---
+++ src/vendors/slomux/index.ts
@@ -0,0 +1,17 @@
+export const createStore = (
+  reducer: (state: number, action: ActionWithPayload) => number,
+  initialState: number
+) => {
+  let currentState = initialState;
+  const listeners: Array<Function> = [];
+
+  const getState = () => currentState;
+  const dispatch = (action: ActionWithPayload) => {
+    currentState = reducer(currentState, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener: Function) => listeners.push(listener);
+
+  return { getState, dispatch, subscribe };
+};
